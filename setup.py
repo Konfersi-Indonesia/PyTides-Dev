bbdc2e5f020b894a8468bb9e636ccde10a8642fb
Konfersi Indonesia <82978589+konfersiid@users.noreply.github.com>
2022-02-09 03:32:39 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='pytides',
       description='Tidal analysis and prediction library.',
       version='0.0.4',
-      author='Sam Cox',
+      author='Sam Cox & Satria Ginanjar',
       author_email='sam.cox@cantab.net',
-      url='http://github.com/sam-cox/pytides',
+      url='https://github.com/Konfersi-Indonesia/PyTides-Dev',
       packages=['pytides'],
       install_requires=['numpy>=1.8','scipy>=0.11'],
       license='MIT')
